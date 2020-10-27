d3bcc555ee83777b107c4649330cfd37e9b1d75f
Vanessacor <vanesapcbarros@gmail.com>
2020-10-27 12:35:43 +0100

    improve UI and add fecha de creacion

--- nueva-cita.php
+++ nueva-cita.php
@@ -9,8 +9,8 @@
     <body>
         <header class="container">
             <h1>Nueva Consulta</h1>
-            <button type="button" class="btn btn-primary">
-                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-left" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <button type="button" id="back-button" class="btn btn-primary" aria="volver a pagina principal">
+                <svg width="3em" height="3em" viewBox="0 0 16 16" id="flecha" class="bi bi-arrow-left" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
                 </svg>
           </button>
@@ -26,7 +26,7 @@
                 <input type="text" class="form-control" placeholder="Tema de Consulta" aria-label="tema de consulta" aria-describedby="basic-addon1">
             </div>
             <?php
-                echo "<p> Fecha de creacion: " . date("Y/m/d") . "  " . date("h:i") . "</p>";
+                echo "<p id=fecha> Fecha de creacion: " . date("Y/m/d") . "  " . date("h:i") . "</p>";
                 
             ?>
             <div class="form-group" id="buttons">
