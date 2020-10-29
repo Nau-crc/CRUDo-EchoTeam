a066dc36056935d8cc8113e65901f2467c981c0f
Vanessacor <vanesapcbarros@gmail.com>
2020-10-29 15:38:40 +0100

    add form to editarCita

---
+++ src/EditarCita.php
@@ -0,0 +1,14 @@
+require("Database.php");
+
+
+<!-- if ( ($)) -->
+
+
+<h1>Edita Cita</h1>
+
+<form action="CrearCita.php" method="post">
+    <input type="text" name="nameCoder">
+    <input type="text" name="temaProblema">
+
+    <input type="submit" value="Crear">
+</form>
