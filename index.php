52eae0c4d8aa1de973c8ac832f4861baaa4c80db
Carmenprz <carmenp.gomez99@gmail.com>
2020-10-29 14:34:27 +0100

    modificar index php

--- index.php
+++ index.php
@@ -4,15 +4,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Intro DB</title>
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
+    
+<header>
+        <img id="logo" src="https://i.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U" alt="perro feo"> 
+
+        <h1 class="title">Citas previstas</h1>  
+</header>
+
+    <div class="container">
+    <ul class="cita">
+        <!-- <li class="list-group-item" id="coderName">Quim</li>
+        <li class="list-group-item" id="tema">Clean Code</li>
+        <li class="list-group-item" id="botonEditar">Editar</li>
+        <li class="list-group-item" id="botonBorrar">Borrar</li> -->
+    </ul>
+    </div> 
 
     <?php
-    require("src/ListStudents.php");
+    require("src/ListaCitas.php");
 
     ?>
+    
 </body>
-
 </html>
