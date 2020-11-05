f4ee04cfd20ffc6a92fa8e62dc7ebe04f9941fc8
Laura <laura.rodcarrrion@gmail.com>
2020-11-05 15:03:05 +0100

    boton de crear a index.php

--- src/Views/CrearConsulta.php
+++ src/Views/CrearConsulta.php
@@ -12,11 +12,12 @@
 <body>
     <header class="container">
         <h1>Nueva Consulta</h1>
+        <a href= "index.php">
         <button type="button" id="back-button" class="btn btn-primary" aria="volver a pagina principal">
             <svg width="3em" height="3em" viewBox="0 0 16 16" id="flecha" class="bi bi-arrow-left" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
             </svg>
-        </button>
+        </button></a>
     </header>
     <main class="container" id="main">
         <form action='?action=save' method="post">
