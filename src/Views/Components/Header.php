93d48ecbff14ba4b49a2ec997777b2ba788577f0
Carmenprz <carmenp.gomez99@gmail.com>
2020-11-06 11:52:17 +0100

    header

--- src/Views/Components/Header.php
+++ src/Views/Components/Header.php
@@ -1,8 +1,15 @@
 
 <body>
+
     <header>
-            <h1 class="title">Consultorio Echo</h1>  
+        <nav class="navbar navbar-dark" style="background-color:black">
+            <a class="navbar-brand" href="#">
+            <img src="https://i.ibb.co/0Zfj70f/logo.jpg" width="30" height="30" class="d-inline-block align-top" alt="logo perro feo" loading="lazy">
+                Consultorio Echo
+            </a>
+        </nav>
     </header>
     
 </body>
+
 
